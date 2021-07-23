13d8330d519ffcd3561111cb6103358fdd2f1f1b
kmorgport <kmorgan0057@gmail.com>
2021-07-23 10:48:28 -0500

    using portals and fragments

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,22 +2,41 @@ import React from 'react';
 import Card from './Card';
 import Button from './Button'
 import classes from './Error.module.css'
+import ReactDOM from 'react-dom'
+
+const Backdrop = props => {
+    return <div className={classes.backdrop} onClick={props.resetError}/>
+}
+
+const ModalOverlay = props => {
+    return (
+        <Card className={classes.modal}>
+        <header className={classes.header}>
+            <h2>{props.title}</h2>
+        </header>
+        <div classes={classes.content}>
+            <p>{props.alert}</p>
+        </div>
+        <footer className={classes.actions}>
+            <Button onClick={props.resetError}>Okay</Button>
+        </footer>
+    </Card>
+    )
+}
+
 const ErrorModal = (props) =>{
     return (
-        <div>
-        <div className={classes.backdrop} onClick={props.resetError}/>
-        <Card className={classes.modal}>
-            <header className={classes.header}>
-                <h2>{props.title}</h2>
-            </header>
-            <div classes={classes.content}>
-                <p>{props.alert}</p>
-            </div>
-            <footer className={classes.actions}>
-                <Button onClick={props.resetError}>Okay</Button>
-            </footer>
-        </Card>
-        </div>
+        // uses portal to help accessibility as it wont crowd the DOM when rendered 
+        <React.Fragment>
+        {ReactDOM.createPortal(<Backdrop resetError={props.resetError}/>,document.getElementById('backdrop-root'))}
+        {ReactDOM.createPortal(<ModalOverlay 
+        title={props.title}
+        message={props.message}
+        resetError={props.resetError} 
+        />,
+        document.getElementById('overlay-root')
+        )}
+        </React.Fragment>
     )
 }
 export default ErrorModal
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card'
 import Button from '../UI/Button'
 import classes from './AddUser.module.css'
 import ErrorModal from '../UI/ErrorModal'
+// import Wrapper from '../Helpers/Wrapper'
 
 const AddUser = props =>{
     const [enteredUserName, setUsername] = useState('');
@@ -50,7 +51,7 @@ const AddUser = props =>{
     }
 
     return (
-        <div>
+        <React.Fragment>
         {error && <ErrorModal title={error.title} alert={error.alert} resetError={errorHandler}/>}
         <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
@@ -65,7 +66,7 @@ const AddUser = props =>{
             <Button type="submit">Add User</Button>
         </form>
         </Card>
-        </div>
+        </React.Fragment>
     )
 }
 
