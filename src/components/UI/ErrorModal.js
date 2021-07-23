2b7b19cc45f2550b54e949ddf825145315ee9dc8
kmorgport <kmorgan0057@gmail.com>
2021-07-22 22:59:30 -0500

    error modal added

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Card from './Card';
 import Button from './Button'
-import classes from './ErrorModal.module.css'
+import classes from './Error.module.css'
 const ErrorModal = (props) =>{
     return (
+        <div>
+        <div className={classes.backdrop} onClick={props.resetError}/>
         <Card className={classes.modal}>
             <header className={classes.header}>
                 <h2>{props.title}</h2>
@@ -12,8 +14,10 @@ const ErrorModal = (props) =>{
                 <p>{props.alert}</p>
             </div>
             <footer className={classes.actions}>
-                <Button>Okay</Button>
+                <Button onClick={props.resetError}>Okay</Button>
             </footer>
         </Card>
+        </div>
     )
 }
+export default ErrorModal
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,18 +2,30 @@ import React, {useState} from 'react'
 import Card from '../UI/Card'
 import Button from '../UI/Button'
 import classes from './AddUser.module.css'
+import ErrorModal from '../UI/ErrorModal'
 
 const AddUser = props =>{
     const [enteredUserName, setUsername] = useState('');
     const [enteredAge, setAge] = useState('');
+    const [error, setError] = useState();
 
 
     const addUserHandler = event =>{
         event.preventDefault();
         if(enteredUserName.trim().length === 0 || enteredAge.trim().length === 0){
+            setError({
+                title: 'Invalid Input',
+                alert: 'Please enter a valid name and age (non-empty)'
+            })
             return
         }
-        if(+enteredAge < 1) return
+        if(+enteredAge < 1) {
+            setError({
+                title: 'Invalid Input',
+                alert: 'Please enter an age greater than 0'
+            })
+            return
+        }
         const User = {
             key: Math.random().toString(),
             name: enteredUserName,
@@ -33,7 +45,13 @@ const AddUser = props =>{
         setAge(event.target.value);
     }
 
+    const errorHandler = ()=>{
+        setError(null);
+    }
+
     return (
+        <div>
+        {error && <ErrorModal title={error.title} alert={error.alert} resetError={errorHandler}/>}
         <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <div>
@@ -47,6 +65,7 @@ const AddUser = props =>{
             <Button type="submit">Add User</Button>
         </form>
         </Card>
+        </div>
     )
 }
 
